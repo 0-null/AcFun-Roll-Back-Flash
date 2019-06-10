f064aebf3046e977d154c5c6aa4bc25da96208cf
补档PO <25119890+BDPO@users.noreply.github.com>
2019-06-10 13:15:18 +0800

    Add files via upload

---
+++ vac2zac.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        AcFun自动换地址 V3视频版
+// @description 强制切换flash播放器
+// @author      补档PO
+// @version     1.3.1
+
+// @include     http://www.acfun.cn/v/ac*
+
+// ==/UserScript==
+
+window.location.href = window.location.href.replace(/^http:\/\/www.\acfun\.cn\/v\//, 'http:\/\/www.\acfun\.cn\/z\/');
