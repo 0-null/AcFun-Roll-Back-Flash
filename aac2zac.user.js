0f0de8524d25761984dd98e95b7ff4e126c3f2f9
补档PO <25119890+BDPO@users.noreply.github.com>
2019-06-10 13:16:53 +0800

    acfun.cn/a/ac 文章页跳转 z/ac

--- aac2zac.user.js
+++ aac2zac.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name        AcFun自动换地址 V3文章版
-// @description 可跳转回早期旧版文章二级页以及评论区
+// @description 目前可以调用旧版评论区风格，不保证后续可用
+
 // @author      补档PO
 // @version     1.3.2
 
-// @include     http://www.acfun.cn/a/ac*
+// @include     https://www.acfun.cn/a/ac*
 
 // ==/UserScript==
 
-window.location.href = window.location.href.replace(/^http:\/\/www.\acfun\.cn\/a\//, 'http:\/\/www.\acfun\.cn\/z\/');
+window.location.href = window.location.href.replace(/^https:\/\/www.\acfun\.cn\/a\//, 'https:\/\/www.\acfun\.cn\/z\/');
