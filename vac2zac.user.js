b828c1573b863ce342e156c6343e02f78ce6d20f
补档PO <25119890+BDPO@users.noreply.github.com>
2019-06-10 13:18:03 +0800

    acfun.cn/v/ac 视频页跳转 z/ac

--- vac2zac.user.js
+++ vac2zac.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name        AcFun自动换地址 V3视频版
-// @description 强制切换flash播放器
+// @description 视频页面调用flash播放器【已失效】
+
+// @description 目前可以调用旧版评论区风格，不保证后续可用
+
 // @author      补档PO
 // @version     1.3.1
 
-// @include     http://www.acfun.cn/v/ac*
+// @include     https://www.acfun.cn/v/ac*
 
 // ==/UserScript==
 
-window.location.href = window.location.href.replace(/^http:\/\/www.\acfun\.cn\/v\//, 'http:\/\/www.\acfun\.cn\/z\/');
+window.location.href = window.location.href.replace(/^https:\/\/www.\acfun\.cn\/v\//, 'https:\/\/www.\acfun\.cn\/z\/');
