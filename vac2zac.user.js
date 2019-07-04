15a76f9ecd001c982f961fd2be2f3b6b8927f333
补档PO <25119890+BDPO@users.noreply.github.com>
2019-07-04 16:15:26 +0800

    acfun.cn/v/ac 视频页跳转 z/ac

--- vac2zac.user.js
+++ vac2zac.user.js
@@ -4,7 +4,7 @@
 // @description 视频页面调用flash播放器
 // @description 旧版评论区风格，不保证后续可用
 
-// @description 感谢 浮云总是梦 全程指导代码
+// @description 感谢 浮云总是梦 全程指导代码！
 // @author      浮云总是梦 补档PO
 // @version     1.3.3
 
