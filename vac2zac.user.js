1494f1b9e47c55083b44b4ec2d91c1bad5949913
补档PO <25119890+BDPO@users.noreply.github.com>
2019-07-04 16:06:27 +0800

    Update vac2zac.user.js

--- vac2zac.user.js
+++ vac2zac.user.js
@@ -1,14 +1,19 @@
 // ==UserScript==
-// @name        AcFun自动换地址 V3视频版
-// @description 视频页面调用flash播放器【已失效】
+// @name        AcFun自动换地址
 
-// @description 目前可以调用旧版评论区风格，不保证后续可用
+// @description 视频页面调用flash播放器
+// @description 旧版评论区风格，不保证后续可用
 
-// @author      补档PO
-// @version     1.3.1
+// @description 感谢 浮云总是梦 全程指导代码
+// @author      浮云总是梦 补档PO
+// @version     1.3.3
 
-// @include     https://www.acfun.cn/v/ac*
+// @include     https://www.acfun.cn/*
 
 // ==/UserScript==
 
-window.location.href = window.location.href.replace(/^https:\/\/www.\acfun\.cn\/v\//, 'https:\/\/www.\acfun\.cn\/z\/');
+if (window.location.href.indexOf('www.acfun.cn/v') > 0) {
+  window.location.href = window.location.href.replace('www.acfun.cn/v', 'www.acfun.cn/z')
+}
+
+document.getElementById("ACFlashPlayer-re").data = "https://cdn.aixifan.com/flash/player-view-homura.swf"
